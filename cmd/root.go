4acce7cb04809fbe49352161cd840c4c3e99abda
dmclink <135415819+dmclink@users.noreply.github.com>
2026-06-27 12:12:11 +0800

    feat: add delimiter default to viper config and update config path

--- cmd/root.go
+++ cmd/root.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/dmclink/flash-cli/internal/constant"
 	"github.com/dmclink/flash-cli/internal/database"
@@ -17,10 +18,11 @@ import (
 func Execute() error {
 	parsedArgs, err := parser.ParseArgs(os.Args)
 	if err != nil {
-		fmt.Println(fmt.Errorf("Error: reordering args | %w", err))
+		fmt.Println(fmt.Errorf("failed to validate and reorder args | %w", err))
 		// TODO: call usage?, if not calling usage, can optionally handle error in main
 		// TODO: also look up if we should handle errors in main for cobra, since the bottom of this function
 		// returns an error, should it be handled? Cobra automatically pushes to stderr anyway do i need to do anything with this?
+		// TODO: use cobra.CheckError(err) here?
 		os.Exit(1)
 	}
 	os.Args = parsedArgs.Args
@@ -40,13 +42,13 @@ var (
 		Long:  "A CLI program to review and manage flashcards backed by an SQLite database. Strives for simplicity and ease of use to add and review. Extensible via plugins.",
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 			if os.Getuid() == 0 {
-				return fmt.Errorf("Error: do not run this application as root/sudo.")
+				return fmt.Errorf("do not run this application as root/sudo.")
 			}
 
 			var err error
 			DB, err = database.OpenAndInitDatabase()
 			if err != nil {
-				return fmt.Errorf("Error: Opening and initializing database | %w", err)
+				return fmt.Errorf("failed to open and initialize database | %w", err)
 			}
 
 			return nil
@@ -75,6 +77,8 @@ func init() {
 
 // TODO: this is boilerplate from the cobra docs, review later if we ever used config files and either delete this block or delete this comment
 func initConfig() {
+	viper.SetDefault("delimiter", "::")
+
 	if cfgFile != "" {
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
@@ -83,10 +87,11 @@ func initConfig() {
 		home, err := os.UserHomeDir()
 		cobra.CheckErr(err)
 
-		// Search config in home directory with name ".cobra" (without extension).
-		viper.AddConfigPath(home)
+		configPath := filepath.Join(home, ".config", constant.APP_NAME)
+
+		viper.SetConfigName("config")
 		viper.SetConfigType("yaml")
-		viper.SetConfigName(".cobra")
+		viper.AddConfigPath(configPath)
 	}
 
 	viper.AutomaticEnv()
