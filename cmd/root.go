36f1111378b329823d5e613a409f1fdf732f99c6
dmclink <135415819+dmclink@users.noreply.github.com>
2026-06-28 12:19:25 +0800

    fix: error handlnig not catching when user supplied config file

--- cmd/root.go
+++ cmd/root.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/dmclink/flash-cli/internal/constant"
 	"github.com/dmclink/flash-cli/internal/parser"
@@ -19,17 +20,15 @@ func Execute(db *sql.DB) error {
 
 	var cfgFile string
 
+	rootCmd := NewRootCmd(db, v, &cfgFile)
+
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra.yaml)")
 	rootCmd.PersistentFlags().Bool("viper", true, "use Viper for configuration")
 
-	err := initConfig(v, cfgFile)
-	if err != nil {
-		return fmt.Errorf("initializing viper config | %w", err)
-	}
-
 	parsedArgs, err := parser.ParseArgs(os.Args)
 	if err != nil {
-		// TODO: call usage?, if not calling usage, can optionally handle error in main
+		// TODO: call usage for parsed command? would need some map or something to lookup command funcs
+		rootCmd.Usage()
 		return fmt.Errorf("failed to validate and reorder args | %w", err)
 	}
 	os.Args = parsedArgs.Args
@@ -43,18 +42,23 @@ func Execute(db *sql.DB) error {
 	return rootCmd.ExecuteContext(ctx)
 }
 
-var rootCmd = &cobra.Command{
-	Use:   constant.APP_NAME,
-	Short: "Flashcard review and management program",
-	Long:  "A CLI program to review and manage flashcards backed by an SQLite database. Strives for simplicity and ease of use to add and review. Extensible via plugins.",
-	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-		// TODO: initialize viper config here
-		return nil
-	},
-	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: run the default command when calling root by itself, likely reviewCmd
-	},
-	Version: "0.1.0",
+func NewRootCmd(db *sql.DB, v *viper.Viper, cfgFile *string) *cobra.Command {
+	return &cobra.Command{
+		Use:   constant.APP_NAME,
+		Short: "Flashcard review and management program",
+		Long:  "A CLI program to review and manage flashcards backed by an SQLite database. Strives for simplicity and ease of use to add and review. Extensible via plugins.",
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			err := initConfig(v, cfgFile)
+			if err != nil {
+				return fmt.Errorf("initializing viper config | %w", err)
+			}
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			// TODO: run the default command when calling root by itself, likely reviewCmd
+		},
+		Version: "0.1.0",
+	}
 }
 
 // func init() {
@@ -65,27 +69,28 @@ var rootCmd = &cobra.Command{
 // }
 
 // TODO: this is boilerplate from the cobra docs, review later if we ever used config files and either delete this block or delete this comment
-func initConfig(v *viper.Viper, cfgFile string) error {
+func initConfig(v *viper.Viper, cfgFile *string) error {
 	v.SetDefault(constant.VIPER_KEY_DELIMITER, "::")
 
 	var configPath string
-	if cfgFile != "" {
-		v.SetConfigFile(cfgFile)
+	configName := "config"
+	configType := "yaml"
+	if *cfgFile != "" {
+		configPath = filepath.Dir(*cfgFile)
+		configName = strings.TrimSuffix(filepath.Base(*cfgFile), filepath.Ext(*cfgFile))
+		configType = strings.TrimPrefix(filepath.Ext(*cfgFile), ".")
 	} else {
-		// Find home directory.
-
 		home, err := os.UserHomeDir()
 		if err != nil {
 			return fmt.Errorf("failed to find home directory for os | %w", err)
 		}
 
 		configPath = filepath.Join(home, ".config", constant.APP_NAME)
-
-		v.SetConfigName("config")
-		v.SetConfigType("yaml")
-		v.AddConfigPath(configPath)
 	}
 
+	v.SetConfigName(configName)
+	v.SetConfigType(configType)
+	v.AddConfigPath(configPath)
 	v.AutomaticEnv()
 
 	err := v.ReadInConfig()
@@ -99,7 +104,7 @@ func initConfig(v *viper.Viper, cfgFile string) error {
 		}
 
 		if err := v.SafeWriteConfig(); err != nil {
-			return fmt.Errorf("failed creatign default config | %w", err)
+			return fmt.Errorf("failed creating default config | %w", err)
 		}
 
 		return nil
